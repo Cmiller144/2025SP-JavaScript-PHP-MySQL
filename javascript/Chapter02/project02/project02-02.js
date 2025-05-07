c1976bc0a0f71321d1f84aff4f6ed268c75f3fbc
Corey <cojmiller@lc.edu>
2025-05-07 14:14:38 -0500

    Initial commit

---
+++ javascript/Chapter02/project02/project02-02.js
@@ -0,0 +1,22 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: Jade Bowker
+      Date:   02/14/2025
+
+      Filename: project02-02.js
+ */
+
+function verifyForm() {
+      let name = document.getElementById("name").value;
+      let email = document.getElementById("email").value;
+      let phone = document.getElementById("phone").value;
+
+      (name && email && phone) ?
+      window.alert("Thank you!") :
+      window.alert("Please fill in all fields");
+}
+
+document.getElementById("submit").addEventListener("click", verifyForm);
